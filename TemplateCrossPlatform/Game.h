f93f3f8b05d85e325987eee1a91326833fbf4342
crocha5 <43393319+crocha5@users.noreply.github.com>
2019-05-07 23:33:36 -0700

    Add files via upload

---
+++ TemplateCrossPlatform/Game.h
@@ -0,0 +1,36 @@
+#ifndef Game_hpp
+#define Game_hpp
+
+#include "AppComponent.h"
+#include "AnimatedRect.h"
+#include "Timer.h"
+
+class Game: public AppComponent, private Timer{
+    TexRect* mushroom;
+    Rect* projectile;
+    
+    TexRect* mangoPod;
+    
+    bool projectileVisible;
+    bool mushroomVisible;
+    bool up;
+    bool left;
+    bool hit;
+    float theta;
+    float deg;
+    bool bounceUp;
+    bool gravity;
+public:
+    AnimatedRect* explosion;
+    Game();
+    
+    void draw() const ;
+    void handleKeyDown(unsigned char, float, float);
+    
+    void action();
+
+    ~Game();
+
+};
+
+#endif 
